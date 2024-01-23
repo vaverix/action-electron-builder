e00cd5161e50db2f2f71f70776ceb5f979307e56
vaverix <vaverix@o2.pl>
2024-01-23 14:45:23 +0100

    Update index.js to add yarn network timeout

--- index.js
+++ index.js
@@ -106,7 +106,7 @@ const runAction = () => {
 	setEnv("ADBLOCK", true);
 
 	log(`Installing dependencies using ${useNpm ? "NPM" : "Yarn"}…`);
-	run(useNpm ? "npm install" : "yarn", pkgRoot);
+	run(useNpm ? "npm install" : "yarn install --network-timeout 600000", pkgRoot);
 
 	// Run NPM build script if it exists
 	if (skipBuild) {
